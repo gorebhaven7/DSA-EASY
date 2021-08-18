2be5f35af62f77e78cc07609a323de84353bb138
Bhaven <gorebhaven7@gmail.com>
2021-08-18 21:23:03 +0530

    first commit

--- Queue/Number of Students unable to Eat Lunch/Solution.py
+++ Queue/Number of Students unable to Eat Lunch/Solution.py
@@ -1,13 +1,18 @@
-import collections
 
 class Solution:
-    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:     
-        
-        count = collections.Counter(students)
-        for i,s in enumerate(sandwiches):
-            if not count[s]:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:        
+        count = 0
+        while count < len(students) :
+            if len(students) == 0:
                 break
-            count[s] -= 1
-        else:
-            i = len(sandwiches)
-        return len(sandwiches) - i
+            
+            else:
+                if students[0] == sandwiches[0]:
+                    count = 0
+                    students.pop(0)
+                    sandwiches.pop(0)
+                else:
+                    count += 1
+                    students.append(students.pop(0))
+                    
+        return len(students)
